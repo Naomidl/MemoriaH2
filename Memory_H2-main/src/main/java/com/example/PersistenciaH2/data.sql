b85ff807ebb5c8730de3209b62d63ec4e84b01ba
Naomidl <naomideleon@emilianisomascos.edu.gt>
2024-07-09 00:24:05 -0600

    Archivo subido

---
+++ Memory_H2-main/src/main/java/com/example/PersistenciaH2/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO countries (id, name) VALUES (1, 'Corea del Sur');
+INSERT INTO countries (id, name) VALUES (2, 'Japon');
+INSERT INTO countries (id, name) VALUES (3, 'Costa ´Rica');
+INSERT INTO countries (id, name) VALUES (4, 'Turquia');
+INSERT INTO countries (id, name) VALUES (5, 'Canada');
